f8ae3f09518830e58fecce99527b910288e071af
Ben Gordon <benharrisgordon@gmail.com>
2021-04-07 12:03:55 +0200

    color picker update

--- src/Playground.jsx
+++ src/Playground.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import { CompactPicker } from 'react-color';
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { ChromePicker } from 'react-color';
 import "./Playground.css";
 
 function Playground({ x, y, blur, spread, opacity, color }) {
   const [boxColor, setBoxColor] = useState("#dedede");
   const [backgroundColor, setBackgroundColor] = useState("#fff");
+  let [showBoxColorPicker, setShowBoxColorPicker] = useState(false);
+  let [showBackgroundColorPicker, setShowBackgroundColorPicker] = useState(false);
 
   return <div className="playground">
     <div className="playground-container">
@@ -23,12 +24,38 @@ function Playground({ x, y, blur, spread, opacity, color }) {
       </div>
 
       <div className="playground-controls-container">
-        <div className="app__control">
+        <div className="playground__control">
+
+
           <div className="box-color-container">
-            <CompactPicker color={boxColor} onChange={e => setBoxColor(e.hex)} />
+            <button
+              onClick={() => setShowBoxColorPicker(showBoxColorPicker = !showBoxColorPicker)}
+              // onBlur={() => setShowBoxColorPicker(showBoxColorPicker = !showBoxColorPicker)}
+            >
+              {showBoxColorPicker ? 'set box color' : 'choose box color'}
+            </button>
+            {showBoxColorPicker && (
+              <ChromePicker
+                className="chrome-picker-palette"
+                color={boxColor}
+                onChange={e => setBoxColor(e.hex)}
+              />
+            )}
           </div>
-          <div className="background-color-container">
-             <CompactPicker color={backgroundColor} onChange={e => setBackgroundColor(e.hex)} />
+
+          <div className="box-color-container">
+            <button
+              onClick={() => setShowBackgroundColorPicker(showBackgroundColorPicker = !showBackgroundColorPicker)}
+            >
+              {showBackgroundColorPicker ? 'set background color' : 'choose background color'}
+            </button>
+            {showBackgroundColorPicker && (
+              <ChromePicker
+                className="chrome-picker-palette"
+                color={backgroundColor}
+                onChange={e => setBackgroundColor(e.hex)}
+              />
+            )}
           </div>
         </div>
       </div>
